0c773213168ebd66f339f8a87330623f4960bde9
Muhammad Shalahuddin <shalahuddinm002@gmail.com>
2025-04-12 22:05:12 +0800

    Membuat tab certificates dan memperbaiki tab techs

--- src/js/about.js
+++ src/js/about.js
@@ -1,14 +1,36 @@
-const tabButtons = document.querySelectorAll('#tab-buttons [data-tab-button]');
-const tabContents = document.querySelectorAll('#tab-contents [data-tab-content]');
+const contentCertificates = document.querySelector('#content-certificates');
 
-tabButtons.forEach(btn => {
-    btn.addEventListener('click', () => {
-        tabButtons.forEach(b => b.classList.remove('border-b', 'text-white'));
-        tabContents.forEach(c => c.classList.add('hidden'));
+async function loadData() {
+    try {
+        const response = await fetch('/src/json/certificates.json');
+        jsonData = await response.json();
+        populateData(jsonData);
+        console.log(jsonData);
+    } catch (error) {
+        console.error('Gagal memuat data:', error);
+    }
+}
 
-        btn.classList.add("border-b", "text-white");
-        const target = btn.getAttribute("data-tab-button");
+function populateData(data) {
+    contentCertificates.innerHTML = ''; // Kosongkan list
 
-        document.querySelector(`#tab-contents [data-tab-content="${target}"]`).classList.remove('hidden');
+    data.reverse(); // Balik urutan data
+
+    data.forEach((item, index)  => {
+        const row = document.createElement('a');
+        row.innerHTML = `
+                    <div class="border-b border-secondary p-3 mb-3 hover:border-white hover:text-white hover:bg-neutral-800 rounded-t-lg transition duration-300 ease-in-out">
+                        <div class="flex items-center justify-between">
+                            <p class="text-white font-semibold truncate">${item.title}</p>
+                            <p class="ms-2">${item.year}</p>
+                        </div>
+                        <p>${item.by}</p>
+                    </div>
+                `;
+        row.href = item.link; // Set link untuk setiap item
+        row.target = '_blank'; // Buka link di tab baru
+        contentCertificates.appendChild(row);
     });
-});
+}
+
+loadData();
